fc7adf6958dece19c3c1027ce465d272689311a7
Warrior Pham <ptdat.19it6@vku.udn.vn>
2023-09-23 21:21:18 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,19 @@
+
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import initWebRoutes from './route/web';
+require('dotenv').config();
+
+let app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+viewEngine(app);
+initWebRoutes(app);
+let port = process.env.PORT || 6969;
+//Port =  undefinded => port = 6969;
+
+app.listen(port, () => {
+  //callback
+  console.log("backend Nodejs is runing  on the port:"+ port)
+});
