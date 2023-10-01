4a47812a370a338a2a1a84d278a1e4db004af7d7
Warrior Pham <ptdat.19it6@vku.udn.vn>
2023-10-01 10:01:38 +0700

    41

--- src/route/web.js
+++ src/route/web.js
@@ -18,7 +18,7 @@ let initWebRoutes = (app) => {
   router.get("/api/get-all-users", userController.handleGetAllUsers);
   router.post("/api/create-new-user", userController.handleCreateNewUser);
   router.put("/api/edit-user", userController.handleEditUser);
-  router.delete("/api/delete-user", userController.handleDeleteUser);//restAPI
+  router.delete("/api/delete-user", userController.handleDeleteUser); //restAPI
 
   return app.use("/", router);
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -102,23 +102,25 @@ let createNewUser = (data) => {
             if (check === true) {
                 resolve({
                     errCode: 1,
-                    message:'Your email is already in used,Plz try another email'
+                    errMessage:'Your email is already in used,Plz try another email'
+                })
+            } else {
+                let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+                await db.User.create({
+                    email: data.email,
+                    password: hashPasswordFromBcrypt,
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    address: data.address,
+                    phonenumber: data.phonenumber,
+                    gender: data.gender === '1' ? true : false,
+                    roleId: data.roleId
+                })
+                resolve({
+                    errCode: 0,
+                    message: 'OK'
                 })
             }
-            let hashPasswordFromBcrypt = await hashUserPassword(data.password);
-            await db.User.create({
-                email: data.email,
-                password: hashPasswordFromBcrypt,
-                firstName: data.lastName,
-                address: data.address,
-                phonenumber: data.phonenumber,
-                gender: data.gender === '1' ? true : false,
-                roleId: data.roleId
-            })
-            resolve({
-                errCode: 0,
-                message: 'OK'
-            })
         } catch (error) {
             reject(error)
         }
@@ -161,6 +163,7 @@ let updateUserData = (data) => {
                 user.firstName = data.firstName;
                 user.lastName = data.lastName;
                 user.address = data.address;
+                user.roleId= data.roleId
 
                 await user.save();
 
