020c28d8313fbcca3ce6116622e7dda7f2115e6c
Warrior Pham <ptdat.19it6@vku.udn.vn>
2023-10-14 11:37:46 +0700

    #65

---
+++ src/models/markdown.js
@@ -0,0 +1,23 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Markdown extends Model {
+
+    static associate(models) {
+      }
+  };
+  Markdown.init({
+    contentHTML: DataTypes.TEXT('long'),
+    contentMarkdown: DataTypes.TEXT('long'),
+    description:DataTypes.TEXT('long'),
+    doctorId: DataTypes.INTEGER,
+    specialtyId: DataTypes.INTEGER,
+    clinicId: DataTypes.INTEGER,
+  }, {
+    sequelize,
+    modelName: 'Markdown',
+  });
+  return Markdown;
+};
