f67ca99ba7bb1cbafd924e4965aa23bfa3bc2921
Warrior Pham <ptdat.19it6@vku.udn.vn>
2023-10-27 18:52:47 +0700

    #78

---
+++ src/models/doctor_infor.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Doctor_Infor extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Doctor_Infor.init({
+    doctorId: DataTypes.INTEGER,
+    provinceId: DataTypes.STRING,
+    priceId: DataTypes.STRING,
+    paymetId: DataTypes.STRING,
+    nameClinic:DataTypes.STRING,
+    addressClinic: DataTypes.STRING,
+    note: DataTypes.STRING,
+    count: DataTypes.INTEGER,
+  }, {
+    sequelize,
+    modelName: 'Doctor_Infor',
+  });
+  return Doctor_Infor;
+};
